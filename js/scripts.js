ead5ae52939367efdedf812e45005501b391c187
Dan Leatherman <dleatherman@gmail.com>
2012-08-21 10:15:33 -0400

    remove return false placeholder in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
 $(function(){
-  $('.arm').click( function(){
-    return false;
-  });
   $('.arm:nth-child(1)').click( function(){
     $(this).parent('nav').toggleClass('active');
     return false;
